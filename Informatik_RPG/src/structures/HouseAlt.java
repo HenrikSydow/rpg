10720b693f1a97eb23b45d935a7e868707435419
ruben.wisst <ruben.wisst@pc-if01-16.ad.tms-hl.org>
2021-03-09 11:06:35 +0100

    nochma

---
+++ Informatik_RPG/src/structures/HouseAlt.java
@@ -0,0 +1,25 @@
+package structures;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+
+import main.EntityHandler;
+
+public class HouseAlt extends Structure{
+	
+	public HouseAlt(int x, int y, EntityHandler entityHandler) {
+		super(x, y, new Dimension(500, 700), Toolkit.getDefaultToolkit().createImage("res\\scenery\\house.png"), entityHandler);
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public Rectangle getGroundBounds() {
+		return new Rectangle(x+size.width/20, y+size.height/3, size.width-size.width/10, size.height-size.height/3);
+	}
+
+}
